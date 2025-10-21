4f8830c802d1cc8e472aedf03c6b0fecef406a22
Arthur Gomes <gomesarthursevero6@gmail.com>
2025-10-20 23:48:10 -0300

    update nos arquivos de index e js

--- Hora do Sistema/script.js
+++ Hora do Sistema/script.js
@@ -1,19 +1,43 @@
 function carregar(){
-    let msg = document.getElementById('msg')
-    const img = document.getElementById('img')
-    const data = new Date()
-    let hora = data.getHours()
-    let minutos = data.getMinutes()
-    if (hora >= 0 && hora < 12){
-        msg.innerHTML = `Agora é ${hora}h ${minutos} min\n BOM DIA!!!`
-        img.src = 'img/dia-redimensionado.jpg'
-    } else if (hora >= 12  && hora <= 18){
-        msg.innerHTML = `Agora é ${hora}h ${minutos} min\n BOA TARDE!!!`
-        img.src = 'img/tarde-redimensionado.jpg'
-        document.body.style.background = '#efb810'
-    } else {
-        msg.innerHTML = `Agora é ${hora}h ${minutos} min\n BOA NOITE!!!`
-        document.body.style.background = '#03318c'
-        img.src = 'img/noite-redimensionado.jpg'
+      const msg = document.getElementById('msg')
+      const img = document.getElementById('img')
+      const tempoH = document.getElementById('hora')
+      const tempoM = document.getElementById('minuto')
+      const tempoS = document.getElementById('segundo')
+
+      function atualizarRelogio() {
+        const data = new Date()
+        let hora = data.getHours()
+        let minutos = data.getMinutes()
+        let segundos = data.getSeconds()
+
+        // Adiciona zero à esquerda
+        if (hora < 10) hora = '0' + hora
+        if (minutos < 10) minutos = '0' + minutos
+        if (segundos < 10) segundos = '0' + segundos
+
+        tempoH.textContent = hora
+        tempoM.textContent = minutos
+        tempoS.textContent = segundos
+
+        // Muda o fundo e imagem conforme a hora
+        if (data.getHours() >= 6 && data.getHours() < 12) {
+          
+          document.body.style.background = '#f6d55c'
+          img.src = 'img/manha-redimensionado.jpg'
+        } else if (data.getHours() >= 12 && data.getHours() < 18) {
+          
+          document.body.style.background = '#efb810'
+          img.src = 'img/tarde-redimensionado.jpg'
+        } else {
+    
+          document.body.style.background = '#03318c'
+          img.src = 'img/noite-redimensionado.jpg'
+        }
+      }
+
+      // Atualiza imediatamente e a cada segundo
+      atualizarRelogio()
+      setInterval(atualizarRelogio, 1000)
     }
-}
+
